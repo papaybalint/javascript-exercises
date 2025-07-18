2c832279c97969b1cac58d2f96c195f0599dbd6d
Balint Papay <146977619+papaybalint@users.noreply.github.com>
2025-07-18 18:33:28 +0200

    Fullra kesz

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const fahren =  (fahrenheit -32 ) / 1.8
+  return Math.round(fahren * 10) /10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const celsiuss =  (celsius * 1.8) + 32
+  return Math.round(celsiuss * 10) / 10
 };
 
 // Do not edit below this line
